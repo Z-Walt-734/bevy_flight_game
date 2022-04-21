c85bc9c4b887e3a83edaad046f7370340b4863aa
Z-Walt-734 <43279092+Z-Walt-734@users.noreply.github.com>
2022-04-21 09:48:08 -0500

    Complete Redesign

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,176 @@
+use bevy::prelude::shape as bevy_shape;
 use bevy::prelude::*;
-use std::ops::Add;
+// use bevy_rapier3d::prelude::shape as rapier_shape;
+// use bevy_rapier3d::prelude::*;
 
-const TIME_STEP: f32 = 1.0 / 60.0;
+// rotation steps
+// const TIME_STEP: f32 = 1.0 / 60.0;
+// how "slippery" slow downs are
+const DECELERATION_SLIDE: f32 = 1.2;
+// how far ships bank when max turn
+const MAX_BANK_ANGLE: f32 = 1.0;
 
 #[derive(Component)]
-struct FlightControls;
+struct Player;
+#[derive(Component)]
+struct TranslationAcceleration {
+    accelerations: Vec3,
+}
+// #[derive(Component)]
+// struct RotationAcceleration {
+//     accelerations: Quat,
+// }
+#[derive(Component)]
+struct TransformAcceleration {
+    translation: TranslationAcceleration,
+    // rotation: RotationAcceleration,
+}
+#[derive(Component)]
+struct IsManeuvering(bool);
+#[derive(Component)]
+struct ShipSpecs {
+    max_speeds: Vec3,
+    acceleration_rates: Vec3,
+}
 
-// fn flight(time: Res<Time>, mut query: Query<&mut Transform, With<FlightControls>>) {
-//     for mut transform in query.iter_mut() {
-//         transform.rotation *= Quat::from_rotation_x(1.1 * time.delta_seconds());
-//         transform.rotation *= Quat::from_rotation_y(1.2 * time.delta_seconds());
-//         transform.rotation *= Quat::from_rotation_z(1.3 * time.delta_seconds());
+// Bundles
+#[derive(Bundle)]
+struct PlayerBundle {
+    _p: Player,
+    #[bundle]
+    transform: (Transform, GlobalTransform),
+    acceleration: TransformAcceleration,
+    is_maneuvering: IsManeuvering,
+    ship_specs: ShipSpecs,
+}
+
+#[derive(Bundle)]
+struct PlayerBox {
+    transform: Transform,
+}
+
+impl Default for PlayerBundle {
+    fn default() -> PlayerBundle {
+        PlayerBundle {
+            _p: Player,
+            transform: (
+                Transform::from_xyz(0.0, 0.0, 1.0),
+                GlobalTransform::identity(),
+            ),
+            acceleration: TransformAcceleration { ..default() },
+            is_maneuvering: IsManeuvering(false),
+            ship_specs: ShipSpecs { ..default() },
+        }
+    }
+}
+
+impl Default for TransformAcceleration {
+    fn default() -> TransformAcceleration {
+        TransformAcceleration {
+            translation: TranslationAcceleration { ..default() },
+            // rotation: RotationAcceleration { ..default() },
+        }
+    }
+}
+
+impl Default for TranslationAcceleration {
+    fn default() -> TranslationAcceleration {
+        TranslationAcceleration {
+            accelerations: Vec3::ZERO,
+        }
+    }
+}
+
+// impl Default for RotationAcceleration {
+//     fn default() -> RotationAcceleration {
+//         RotationAcceleration {
+//             accelerations: Quat::IDENTITY,
+//         }
 //     }
 // }
 
-fn controls(
+impl Default for ShipSpecs {
+    fn default() -> ShipSpecs {
+        ShipSpecs {
+            // TODO make these unique to each ship
+            // 1.0 should probably be max
+            max_speeds: Vec3::new(1.0, 1.0, 1.0),
+            // point .02 to .05 seems to be a good range
+            acceleration_rates: Vec3::new(0.03, 0.03, 0.03),
+        }
+    }
+}
+
+// Systems
+fn player_control_system(
     input: Res<Input<KeyCode>>,
-    time: Res<Time>,
-    mut query: Query<&mut Transform, With<FlightControls>>,
+    // time: Res<Time>,
+    mut query: Query<(&mut TransformAcceleration, &ShipSpecs), With<Player>>,
 ) {
-    let mut transform = query.single_mut();
-    let mut x_rotation = 0.0;
-    let mut y_rotation = 0.0;
+    // let keys = input.get_pressed();
+    let (mut transform_acceleration, ship_specs) = query.single_mut();
+    let mut is_maneuvering = false;
 
-    if input.pressed(KeyCode::W) {
-        //pull up
-        // delta_transform = delta_transform + Quat::from_rotation_x(-1.0 * time.delta_seconds());
-        transform.rotation *= Quat::from_rotation_x(-1.0 * time.delta_seconds());
-        info!("Push Down");
-    }
-    if input.pressed(KeyCode::S) {
-        //pull up
-        // delta_transform = delta_transform + Quat::from_rotation_x(1.0 * time.delta_seconds());
-        transform.rotation *= Quat::from_rotation_x(1.0 * time.delta_seconds());
-        info!("Pull Up");
-    }
-    if input.pressed(KeyCode::A) {
-        //pull up
-        // delta_transform = delta_transform + Quat::from_rotation_y(1.0 * time.delta_seconds());
-        transform.rotation *= Quat::from_rotation_y(1.0 * time.delta_seconds());
-        info!("Bank Left");
-    }
-    if input.pressed(KeyCode::D) {
-        //pull up
-        // delta_transform = delta_transform + Quat::from_rotation_y(-1.0 * time.delta_seconds())
-        transform.rotation *= Quat::from_rotation_y(-1.0 * time.delta_seconds());
-        info!("Bank Right");
-    }
+    input.get_pressed().for_each(|k| match k {
+        KeyCode::W => {
+            is_maneuvering = true;
 
-    // transform.rotate(Quat::from_rotation_x(x_rotation * TIME_STEP));
-    // transform.rotate(Quat::from_rotation_y(y_rotation * TIME_STEP));
+            if transform_acceleration.translation.accelerations.y > -ship_specs.max_speeds.y {
+                transform_acceleration.translation.accelerations +=
+                    Vec3::new(0.0, -ship_specs.acceleration_rates.y, 0.0);
+            }
+        }
+        KeyCode::A => {
+            is_maneuvering = true;
+
+            if transform_acceleration.translation.accelerations.x < ship_specs.max_speeds.x {
+                transform_acceleration.translation.accelerations +=
+                    Vec3::new(ship_specs.acceleration_rates.x, 0.0, 0.0);
+            }
+        }
+        KeyCode::S => {
+            is_maneuvering = true;
+
+            if transform_acceleration.translation.accelerations.y < ship_specs.max_speeds.y {
+                transform_acceleration.translation.accelerations +=
+                    Vec3::new(0.0, ship_specs.acceleration_rates.y, 0.0);
+            }
+        }
+        KeyCode::D => {
+            is_maneuvering = true;
+
+            if transform_acceleration.translation.accelerations.x > -ship_specs.max_speeds.x {
+                transform_acceleration.translation.accelerations +=
+                    Vec3::new(-ship_specs.acceleration_rates.x, 0.0, 0.0);
+            }
+        }
+        _ => {}
+    });
+    // Slow down if not accelerating
+    if !is_maneuvering {
+        let x = transform_acceleration.translation.accelerations.x;
+        let y = transform_acceleration.translation.accelerations.y;
+
+        if x != 0.0 {
+            transform_acceleration.translation.accelerations.x = x / DECELERATION_SLIDE;
+        }
+        if y != 0.0 {
+            transform_acceleration.translation.accelerations.y = y / DECELERATION_SLIDE;
+        }
+    }
+}
+
+fn move_ship_system(mut query: Query<(&mut Transform, &TransformAcceleration, &ShipSpecs)>) {
+    for (mut transform, transform_acceleration, ship_specs) in query.iter_mut() {
+        transform.translation += transform_acceleration.translation.accelerations;
+
+        let rot_z = (transform_acceleration.translation.accelerations.x / ship_specs.max_speeds.x)
+            * -MAX_BANK_ANGLE;
+        let rot_x = (transform_acceleration.translation.accelerations.y / ship_specs.max_speeds.y)
+            * -MAX_BANK_ANGLE;
+        let rotation_percent = Quat::from_euler(EulerRot::XYZ, rot_x, 0.0, rot_z);
+        transform.rotation = rotation_percent;
+    }
 }
 
 fn setup(
@@ -58,23 +179,25 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    let ship: Handle<Scene> =
-        asset_server.load("LowPolySpaceshipPack/Models/GLTF/LPSP_LuxuryShip.gltf#Scene0");
+    let ship: Handle<Scene> = asset_server.load("ships/Pancake/glTF/Pancake.gltf#Scene0");
     commands
-        .spawn_bundle((
-            Transform::from_xyz(0.0, 0.0, 1.0),
-            GlobalTransform::identity(),
-        ))
+        .spawn_bundle(PlayerBundle {
+            ..Default::default()
+        })
         .with_children(|p| {
             p.spawn_scene(ship);
-        })
-        .insert(FlightControls)
-        .with_children(|p| {
-            p.spawn_bundle(PerspectiveCameraBundle {
-                transform: Transform::from_xyz(0.0, 8.0, 30.0).looking_at(Vec3::ZERO, Vec3::Y),
-                ..default()
-            });
         });
+    // In place in prep for collision camera follower
+    // .with_children(|p| {
+    //     p.spawn_bundle(PerspectiveCameraBundle {
+    //         transform: Transform::from_xyz(0.0, 8.0, 30.0).looking_at(Vec3::ZERO, Vec3::Y),
+    //         ..default()
+    //     });
+    // });
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_xyz(0.0, 8.0, -30.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
 
     commands.spawn_bundle(PointLightBundle {
         transform: Transform::from_xyz(4.0, 5.0, -4.0),
@@ -82,7 +205,7 @@ fn setup(
     });
 
     commands.spawn_bundle(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Plane { size: 50.0 })),
+        mesh: meshes.add(Mesh::from(bevy_shape::Plane { size: 50.0 })),
         material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
         transform: Transform::from_xyz(0.0, -10.0, 0.0),
         ..default()
@@ -95,6 +218,8 @@ fn main() {
     app.add_plugins(DefaultPlugins)
         .add_startup_system(setup)
         // .add_system(flight)
-        .add_system(controls)
+        .add_system(player_control_system)
+        // .add_system(move_camera_system)
+        .add_system(move_ship_system)
         .run();
 }
